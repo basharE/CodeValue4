49c70ea6d800b908f960b2feea585701b47e8b46
CodeValue <nikolai646@gmail.com>
2016-08-24 00:22:43 +0300

    .

--- Queries/Queries/Program.cs
+++ Queries/Queries/Program.cs
@@ -13,9 +13,10 @@ namespace Queries
             var ass = Assembly.Load(@"mscorlib");
             var firstQuere = from type in ass.GetTypes()
                 where type.IsInterface && type.IsPublic
-                orderby type.GetMethods().Count() ascending
+                orderby type.GetMethods().Count() ascending // should be ordered by name
                 select new {InterfaceName = type.Name, NumberofMethods = type.GetMethods().Length};
 
+            // ???
             foreach (var intrfac in firstQuere)
             {
                 //Console.WriteLine(intrfac);
@@ -25,10 +26,12 @@ namespace Queries
             var procceses = Process.GetProcesses();
 
             var secondQuere = from proc in procceses
-                where proc.Threads.Count < 5
+                // where is your checking for running proccesses
+                where proc.Threads.Count < 5 
                 orderby proc.Id
                 select new {proc.ProcessName, proc.Id, proc.BasePriority /*,proc.StartTime*/};
 
+            // ???
             foreach (var process in secondQuere)
             {
                 //Console.WriteLine(process);
@@ -41,12 +44,16 @@ namespace Queries
                 orderby newProcess.Key
                 select newProcess.Key;
 
+            // ???
             foreach (var process in thirQuere)
             {
                 //Console.WriteLine(process);
             }
 
-
+            // Should be:
+            //var query3 = (from process in Process.GetProcesses()
+            //              where CanAccess(process)
+            //              select process.Threads.Count).Sum();
             var helpFourthQuere = from proc in procceses
                 select new {proc.ProcessName, proc.Threads.Count};
 
@@ -54,13 +61,14 @@ namespace Queries
 
             Console.WriteLine($@"Number of Threads :  {fourthQuere}");
         }
-
+        // Wrong answer
         public static void CopyTo(this object myObj, object obj)
         {
             var prop = from propertises in myObj.GetType().GetProperties()
                 where propertises.CanRead
                 select propertises;
 
+            // ??????????
             from propertises in prop
             where propertises.CanWrite
             select new {prop = propertises.SetValue(obj, propertises.GetValue(myObj, null), null)};
